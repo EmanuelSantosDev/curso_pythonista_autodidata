b10e458d9662ffc2ec456f0ee008ff98203c3223
Emanuel Santos <emanuel.santos.developer@gmail.com>
2024-01-22 10:04:56 -0300

    Update

---
+++ 02 - Introdução ao Python/01_variaveis.py
@@ -0,0 +1,22 @@
+# Atribução de Valor à Variável / Alocando Memória
+velocidade_internet = 10
+print(velocidade_internet)
+
+# Números
+velocidade_internet = 20
+
+# Valores Boleanos 
+velocidade_internet = True
+
+# Strings
+velocidade_internet = 'Dez'
+
+# Unpacking
+a,b,c,d = 1,2,3,4
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Descobrindo o Tipo de Variável
+print(type(velocidade_internet)) # <class 'str'>
